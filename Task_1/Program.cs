1568cb8f15d2829a1a6cd6c5f2e57ca782f3f0a0
Владимир Ворона <vvorona665@gmail.com>
2022-07-03 17:13:07 +0300

    Completed Task 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте двумерный массив размером m*x, заполненный случайными вещественными числами
+
+
+
+double [,] CreateArray (int n, int m) // Создание массива 
+{
+    return new double [n,m];
+} 
+
+void FillArray (double [,] array) // Заполнение массива 
+{
+    for (int i = 0; i < array.GetLength(0); i++) // Переменная i идет по строкам 
+    {
+        for (int j = 0; j < array.GetLength(1); j++)   // Переменная j идет по столбцам
+        {
+            array[i,j] = Math.Round(((new Random().NextDouble()) * 20 - 10), 1);
+            
+        }
+    }
+}
+
+void PrintArray (double [,] array) // Вывод массива
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double [,] arr = CreateArray(3,4);
+FillArray(arr);
+PrintArray(arr);
